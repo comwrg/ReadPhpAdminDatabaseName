f7ef49976710229160b96df1c6ffdb36e2932f19
Wrg <xcomwrg@gmail.com>
2017-06-19 15:36:42 +0800

    rename winform name

--- ReadPhpAdminDatabaseName/FormMain.Designer.cs
+++ ReadPhpAdminDatabaseName/FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace ReadPhpAdminDatabaseName
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 302);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
